fa37e1817b78d06a7571b6db58f57a0c1e0afb33
Hasanboy <maqsudaliyevhasanboy9@gmail.com>
2024-08-14 16:39:18 +0500

    Ikkinchi loyixa

--- Salom.c
+++ Salom.c
@@ -1,190 +1,110 @@
 #include <stdio.h>
+#include "./t.c"
 
 
-struct praduct
+void savdo(int joyi, int soni)
 {
-   char name[200];
-   float narx;
-   int xajmi;
-};
+    maxsulot_soni[joyi] = soni;
+}
 
-void view_proect(struct praduct arr[],int size);
-void max(struct praduct arr[],int size);
-void min(struct praduct arr[],int size);
-void answer(struct praduct arr[],int num);
-void check(float a);
+void menyuuchun()
+{
+    for (int i = 0; i < LEN; i++)
+    {
+        printf("%d. %s\t\t%d so'm\n", i, maxsulotlar[i], maxsulot_narxi[i]);
+    }
+}
 
+int malumot(char ma[], int dan, int gacha)
+{
+    int ja;
+    int bel = 0;
+    
+    do
+    {
+        if (bel > 0)
+        {
+            printf("\nbunday variant yo'q iltmos (%d-dan %d-gacha) orsida tanlang\n", dan, gacha);
+        }
+        printf("%s", ma);
+        scanf("%d", &ja);
+        bel++;  
+    } while (ja < dan || ja >= gacha);
+
+    return ja;
+}
+
+
+int malumot2(char ma[], int dan, int gacha)
+{
+    int ja;
+    int bel = 0;
+    do
+    {
+        if (bel > 0)
+        {
+            printf("\nBuncha miqdor bizda mavjut emas (%d-dan %d-gacha) orsida tanlang\n", dan, gacha);
+        }
+        printf("%s", ma);
+        scanf("%d", &ja);
+        bel++;
+    } while (ja < dan || ja >= gacha);
+
+    return ja;
+}
+
+int hisob()
+{
+
+    int sum = 0;
+    for (int i = 0; i < LEN; i++)
+    {
+        sum += maxsulot_soni[i] * maxsulot_narxi[i];
+    }
+
+    return sum;
+}
+
+void jarayon()
+{
+    int bel = 1;
+    printf("Assalomu alaykum, Oshxonamizga hushkelibsiz\n");
+    while (bel > 0)
+    {
+        menyuuchun();
+        int joyi = malumot("nima kerak?\n(iltmos menuda bor maxsulotni birni talang): ", 0, LEN);
+        int soni = malumot2("nechta olasiz?\n", 1, 100);
+        savdo(joyi, soni);
+        printf("yana biror narsa olasizmi? 0-yoq, 1-ha: ");
+        scanf("%d", &bel);
+    }
+    chek(13);
+}
+
+void chek(int tekshirish)
+{
+    printf("\n\n\t\t\tChek\n|------------------------------------------------|");
+    for (int i = 0; i < LEN; i++)
+    {
+    
+        if (maxsulot_soni[i] > 0)
+            printf("\n| %d %s \t%d * %d = %d so'm |\n", i + 1, maxsulotlar[i], maxsulot_soni[i], maxsulot_narxi[i], maxsulot_soni[i] * maxsulot_narxi[i]);
+    }
+    printf("|-----------------------------\n");
+    int summa = hisob();
+    printf("| JAMI %d so'm %d foiz  |\n", summa, tekshirish);
+    if (tekshirish > 0)
+    {
+        printf("| TOLOV: %d so'm        |\n|-----------------------------\nYana keling!!!\n", summa - summa * tekshirish / 100);
+    }
+    else
+    {
+        printf("TOLOV: %d so'm\n\n\t| Yana keling!!! |", summa);
+    }
+}
 
 int main()
 {
-  
-
-  struct praduct praducts[]=
-  {
-     {"Pepsi 2l",15000,40},
-     {"Pepsi 1l",12000,42},
-     {"Qatiq",4000,50},
-     {"Daftar",2000,54},
-     {"Ruchka",1000,56},
-     {"Tarvuz",23000,65},
-     {"Qovun",24000,60},
-     {"Olma",9000,82},
-     {"Piyoz",4000,100},
-     {"Un",10000,112},
-     {"Shakar",12000,200},
-     {"Gugurt",250,300},
-     {"Tuxum",1500,250},
-     {"Paypoq",5000,85},
-     {"Naushnik simli",20000,22},
-     {"Zaryadnik",15000,100},
-     {"Pamildori",5000,100},
-     {"Bodiring",5000,100}
-  };
-  
-  int n=1;
-  int len=sizeof(praducts)/sizeof(praducts[0]);
-  
-  while(n)
-  {
-      printf("|-----------------------------------------------------|\n");
-      printf("|    Assalomu alaykum do'konimizga hush kelibsiz      |\n");
-      printf("|-----------------------------------------------------|\n");
-      printf("|  Maxsulotlar ro'yxatini ko'rish uchun 1 ni Bosing   |\n");
-      printf("|  Eng qimmat maxsulot 2 ni Bosing                    |\n");
-      printf("|  Eng arzon maxsulot 3 ni Bosing                     |\n");
-      printf("|-----------------------------------------------------|\n\n");
-     
-      int a;
-      
-      printf(">>>>>> :");
-      scanf("%d",&a);
-    
-          switch(a)
-          {
-           case 1:view_proect(praducts,len);break;
-           case 2: max(praducts,len);break;
-           case 3: min(praducts,len);break;
-           default: printf("=======>>>>>>Error 404<<<<<<=====\n");break;
-          }
-             
-              printf("\n\nYana nimadir hohlaysizmi ?\nAgar yana hohlasangiz 1 ni akis holda 0 ni kriting ");
-              scanf("%d",&n);
-  }
-
-   return 0;
-}
-
-
-void view_proect(struct praduct arr[],int size)
-{
-
-  int a,c=1,t;
-  float d=0;
-  
-   printf("|T/R|      Nomi     |      Narxi    | neshtaligi |\n");
-  
-   for(int i=0;i<size;i++)
-   {
-       printf("|%3i|%-15s|%15.2f|%12i|\n",i,arr[i].name,arr[i].narx,arr[i].xajmi);
-   }
-   
-   while(c)
-   {
-    
-     printf("Nima sotib olasiz tartib raqamini kriting \n>>>>>>> ");
-     m:
-     scanf("%d",&a);
-     
-     if(a>17){
-       printf("Xato qayta kiritin \n");
-       goto m;
-     }
-    
-     printf("Neshta olasiz \n>>>>>> ");
-     scanf("%d",&t);
-    
-     if(arr[a].xajmi>t)
-     {
-        d += t*arr[a].narx;
-        arr[a].xajmi=arr[a].xajmi-t;
-     }
-     
-     else{
-      
-          printf("Bizda buncha maxsulot yo'q \n Bizda %d ta maxsulot mavjut olasizmi \n",arr[a].xajmi);
-          printf("Olsangiz 1ni bosing akisxolda 0 ni");
-          scanf("%d",&c);
-          
-          if(c==0){
-            return;
-          }
-         
-          else {
-             d += arr[a].xajmi * arr[a].narx;
-          }
-     }
-     
-     printf("Yana narsa olsangiz 1 ni bosing akis holda 0 ni\n>>>>> ");
-     scanf("%d",&c);
-   }
-
-   check(d);
-
-}
-
-
-void max(struct praduct arr[],int size)
-{
-  struct praduct max=arr[0];
-     for(int i=0;i<size;i++)
-     {
-       if(max.narx<arr[i].narx)
-       {
-          max=arr[i];
-       }
-     }
-    printf("\nEng qimmat maxsulot ==|%-15s|%.2f|\n",max.name,max.narx);
-}
-
-
-
-void min(struct praduct arr[],int size)
-{
-    struct praduct min=arr[0];
-      for(int i=0;i<size;i++)
-      {
-        if(min.narx>arr[i].narx)
-        {
-          min=arr[i];
-        }
-      }
-     printf("\nEng arzon maxsulot ==|%-15s|%.2f|\n",min.name,min.narx);
-}
-
-
-
-void check(float a){
-   
-    float d;
-    
-    printf("\t\t|CHECK|\n");
-    printf("|------------------------------|\n");
-    printf("|%20.2f-So'm|\n",a);
-    
-    f:
-    printf("Pulni to'lang ");
-    scanf("%f",&d);
-
-    if(a-d==0){
-      printf("Harid uchun raxmat");
-    }
-    
-    else if(d-a>0){
-        printf("Qaytimingiz %.2f",a-d);
-    }
-
-    else{
-      printf("Qayta to'lang ");
-      goto f;
-    }
+    jarayon();
+    return 0;
 }
---
+++ t.c
@@ -0,0 +1,44 @@
+
+int LEN = 12;
+
+char maxsulotlar[12][100] = {
+    "Palov",
+    "Somsa",
+    "Sho'rva",
+    "Shashlik",
+    "Mastava",
+    "O'rama",
+    "Monti",
+    "Pepsi",
+    "Coca-cola",
+    "Choy ko'k",
+    "Choy qora",
+    "Perashka + sousi"
+};
+
+int maxsulot_soni[12] = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+};
+int maxsulot_narxi[12] = {
+    25000,
+    5000,
+    15000,
+    13000,
+    15500,
+    8000,
+    5000,
+    13000,
+    13000,
+    3500,
+    3000,
+    1000};
